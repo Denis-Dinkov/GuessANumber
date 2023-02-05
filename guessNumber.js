5d77be6bf9408c5a022f0cffb2d72fb64fce860b
Denis Dinkov <denisdinkov7@gmail.com>
2023-02-06 01:54:45 +0200

    Update guessNumber.js

--- guessNumber.js
+++ guessNumber.js
@@ -7,7 +7,22 @@ let computerGuess = Math.floor(Math.random() * 100);
 
 let recursiveAsyncReadLine = function(){
   readline.question(`Guess the number (0 - 100): `, number =>{
-
+    guess = Number(number);
+    isTrue = false;
+    
+    if(guess <= 100 && guess >= 0){
+      if (guess == computerGuess){
+        console.log('Congrats! You guess it!');
+        isTrue = true;
+      } else if (guess < computerGuess){
+        console.log('Too Low! Try again :)');
+      } else if(guess > computerGuess){
+        console.log('Too High! Try again :)');
+      }
+    } else {
+      console.log('Invalid input! Try again');
+      recursiveAsyncReadLine();
+    }
   })
 }
 recursiveAsyncReadLine()
