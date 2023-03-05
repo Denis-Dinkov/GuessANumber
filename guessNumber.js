d52ad552383fea0618c00be3cb89c0cb803d7941
Denis Dinkov <denisdinkov7@gmail.com>
2023-03-06 00:49:00 +0200

    finished project

--- guessNumber.js
+++ guessNumber.js
@@ -1,28 +1,34 @@
+const { count } = require("console");
+
 const readline = require(`readline`).createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
-
+let counter = 0;
 let computerGuess = Math.floor(Math.random() * 100);
 
-let recursiveAsyncReadLine = function(){
-  readline.question(`Guess the number (0 - 100): `, number =>{
+let recursiveAsyncReadLine = function () {
+  readline.question(`Guess the number (0 - 100): `, (number) => {
     guess = Number(number);
     isTrue = false;
-    
-    if(guess <= 100 && guess >= 0){
-      if (guess == computerGuess){
-        console.log('Congrats! You guess it!');
+
+    if (guess <= 100 && guess >= 0) {
+      if (guess == computerGuess) {
+        console.log("Congrats! You guess it!");
         isTrue = true;
-      } else if (guess < computerGuess){
-        console.log('Too Low! Try again :)');
-      } else if(guess > computerGuess){
-        console.log('Too High! Try again :)');
+      } else if (guess < computerGuess) {
+        console.log("Too Low! Try again :)");
+        counter++;
+        recursiveAsyncReadLine();
+      } else if (guess > computerGuess) {
+        console.log("Too High! Try again :)");
+        counter++;
+        recursiveAsyncReadLine();
       }
     } else {
-      console.log('Invalid input! Try again');
+      console.log("Invalid input! Try again");
       recursiveAsyncReadLine();
     }
-  })
-}
-recursiveAsyncReadLine()
+  });
+};
+recursiveAsyncReadLine();
